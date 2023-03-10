969ea5aa7f96d9ff835ecc9a12747d57a5bdb396
Alin Dan <alindan@Alins-MBP.lan>
2023-03-10 23:03:06 +0200

    classic navbar unedited

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Home from "./components/home/Home"
 import DarkTheme from "./components/dark_theme/DarkTheme"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
         <BrowserRouter>
         
           <Routes>
-          <Route path="/" element={<div><Home /><DarkTheme/></div>} />
+          <Route path="/" element={<div><Navbar/><Home/><DarkTheme/></div>} />
           <Route path="/login" element={<div> <Login /><DarkTheme/></div>} />
           <Route path="/register" element={<div> <Register /><DarkTheme/></div>} />
           </Routes>
