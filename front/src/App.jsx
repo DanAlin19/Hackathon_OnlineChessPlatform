4ab7c82fd63b371dfcff9bb3eab5186af25c0041
robertpetre <petrerobert0402@gmail.com>
2023-04-08 17:20:45 +0300

    sah cu calculatoru

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,69 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import JoinRoom from './components/onboard/joinroom'
-import { ColorContext } from './components/context/colorcontext'
-import Onboard from './components/onboard/onboard'
-import JoinGame from './components/onboard/joingame'
-import ChessGame from './components/chess/ui/chessgame'
-/*
- *  Frontend flow: 
- * 
- * 1. user first opens this app in the browser. 
- * 2. a screen appears asking the user to send their friend their game URL to start the game.
- * 3. the user sends their friend their game URL
- * 4. the user clicks the 'start' button and waits for the other player to join. 
- * 5. As soon as the other player joins, the game starts. 
- * 
- * 
- * Other player flow:
- * 1. user gets the link sent by their friend
- * 2. user clicks on the link and it redirects to their game. If the 'host' has not yet 
- *    clicked the 'start' button yet, the user will wait for when the host clicks the start button.  
- *    If the host decides to leave before they click on the "start" button, the user will be notified
- *    that the host has ended the session. 
- * 3. Once the host clicks the start button or the start button was already clicked on
- *    before, that's when the game starts. 
- * Onboarding screen =====> Game start. 
- * 
- * Every time a user opens our site from the '/' path, a new game instance is automatically created
- * on the back-end. We should generate the uuid on the frontend, send the request with the uuid
- * as a part of the body of the request. If any player leaves, then the other player wins automatically.  
- * 
- */
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import Home from "./components/home/Home"
+import Navbar from "./components/navbar/Navbar"
+import Game from "./components/chess/Chess"
+import Contactus from "./components/contactus/Contactus"
+import ReviewPanel from "./components/reviewpanel/ReviewPanel";
 
 function App() {
 
-  const [didRedirect, setDidRedirect] = React.useState(false)
-
-  const playerDidRedirect = React.useCallback(() => {
-    setDidRedirect(true)
-  }, [])
-
-  const playerDidNotRedirect = React.useCallback(() => {
-    setDidRedirect(false)
-  }, [])
-
-  const [userName, setUserName] = React.useState('')
-
   return (
-    <ColorContext.Provider value={{ didRedirect: didRedirect, playerDidRedirect: playerDidRedirect, playerDidNotRedirect: playerDidNotRedirect }}>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Onboard setUserName={setUserName} />} />
 
-          <Route path="/game/:gameid" exact element={didRedirect ?
-            <React.Fragment>
-              <JoinGame userName={userName} isCreator={true} />
-              <ChessGame myUserName={userName} />
-            </React.Fragment>
-            :
-            <JoinRoom />}>
-          </Route>
-          {/* <Navigate to="/" /> */}
-        </Routes>
-      </Router>
-    </ColorContext.Provider>);
+    <div>
+        <BrowserRouter>
+        
+          <Routes>
+          <Route path="/" element={<div><Navbar/><Home/><Contactus/></div>} />
+          <Route path="/login" element={<div> <Navbar/><Login /></div>} />
+          <Route path="/register" element={<div> <Navbar/><Register /></div>} />
+          <Route path="/chess" element={<div className="relative w-96 items-center"><Game /></div>} />
+          <Route path="/contactus" element={<div> <Navbar/><contactus /></div>} />
+          <Route path="/reviewpanel" element={<div> <Navbar/><ReviewPanel /></div>} />
+          </Routes>
+      
+        </BrowserRouter>
+      </div>
+
+  )
 }
 
-export default App;
+export default App
